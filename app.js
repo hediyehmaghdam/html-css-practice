1ee71f10b1a3dcee23b499ac5008ba322adb23c5
hediyehmaghdam <hdyhmhmdi81@gmail.com>
2023-07-22 21:17:06 +0430

    added semi-colon to js codes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let firstName = "hediyeh"
-let lastName = "mohamadi"
-let fulName = firstName + ' ' + lastName
+let firstName = "hediyeh";
+let lastName = "mohamadi";
+let fulName = firstName + ' ' + lastName;
 
-console. log(fulName)
+console. log(fulName);
