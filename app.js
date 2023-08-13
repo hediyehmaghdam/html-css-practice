cf862f5e99373dc36e40f04988d8ed5a7785f528
hediyehmaghdam <hdyhmhmdi81@gmail.com>
2023-08-13 22:33:43 +0430

    challange01

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-let firstName = "hediyeh";
-let lastName = "mohamadi";
-let fullName = firstName + ' ' + lastName;
+let userAccount = {
+    fullName: 'hediyeh',
+    income: 0,
+    outgo: 0
+}
 
-console. log("Full Name: ", fullName);
+let addIncome = function(account, amount){
+   account.income = account.income + amount
+}
+
+let addOutgo = function(account, outgo){
+    account.outgo = account.outgo + outgo
+}
+
+let getAccountData = function(account) {
+    let balance = account.income - account.outgo
+    return `account name: ${account.fullName} - balance= ${balance}`
+}
+
+addIncome(userAccount, 2000)
+addOutgo(userAccount,250)
+console.log(getAccountData(userAccount))
