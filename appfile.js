c0288d5261cc057b3a70a51108188344274b5d08
hediyehmaghdam <hdyhmhmdi81@gmail.com>
2023-08-27 09:57:33 +0430

    challange 03

--- appfile.js
+++ appfile.js
@@ -1,4 +1,4 @@
-let processing = function(password) {
+let processing = function (password) {
     if (password.length > 8 && !password.includes('12345')) {
         return true
     } else {
@@ -11,6 +11,20 @@ console.log(processing('hediyeh1381'))
 console.log(processing('hedi12345ujeui'))
 console.log(processing('zaqwsx'))
 console.log(processing('za123456'))
-    
+
+const test = ['Book1', 'Book2', 'Book3', 'Book4', 'Book5', 'Book6']
+
+
+test.shift()
+test[5] = 'Book New'
+test.splice(1, 1)
+test.forEach(function(item){
+    console.log(`product Name is: ${item}`)
+})
+
+
+
+
+
 
 
